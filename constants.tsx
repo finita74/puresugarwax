18d6235ba5e7120f9c939fd8b445fe3639f721f9
finita74 <finita74@gmail.com>
2026-05-19 19:33:57 -0400

    rev14

--- constants.tsx
+++ constants.tsx
@@ -14,6 +14,7 @@ export const LOCATION = {
 
 export const ALERTS = [
   "Only Accepting Appointments, No Walk-ins",
+  "We do not offer Leg Sugaring at this time"
 ];
 
 export const SERVICES: ServiceItem[] = [
